453577c4cb7c63cae6a66f8b07a853528a95b8e5
hermes canuto <hermes.canuto@gmail.com>
2023-01-18 10:17:10 -0300

    initial commit

---
+++ cmd/api.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func get_port() string {
+	port := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		port = ":" + val
+	}
+	return port
+}
+
+func hello(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"msg": "Hello World"})
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/api/hello", hello)
+	e.Logger.Fatal(e.Start(get_port()))
+}
